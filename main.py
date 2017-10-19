0f9105fdb2f4b366541b4b464c98eb6364c43cd8
Aikiro <32895215+Aikiro@users.noreply.github.com>
2017-10-19 20:06:59 +0200

    First working prototype
    Fuck yeah im a genious!

--- main.py
+++ main.py
@@ -1,3 +1,34 @@
-game = input("Enter game ID: ")
-mod = input("Enter mod ID: ")
-print("Game ID {0} Mod ID {1}".format(game, mod))
+import cmd, sys, subprocess
+
+class MyPrompt(cmd.Cmd):
+	intro = "Welcome to LibreWorkshop. Type help or ? to list commands.\n"
+	prompt = "> " 
+	file = None
+
+	mods = []
+	steamcdm_path = "E:\\Programing\\SteamCMD\\steamcmd.exe"
+
+	def do_hello(self, arg):
+		"Says hello and your name"
+		if arg:
+			print("Hello {0}.".format(arg))
+		else:
+			print("Hello stranger.")
+
+	def do_addmod(self, arg):
+		"Adds a mod to the mod list. Type the Game ID and the ModID."
+		self.mods.append(arg.split())
+
+	def do_showmods(self, arg):
+		"Shows the mod list."
+		for mod in self.mods:
+			print("Game ID: {0}, Mod ID: {1}".format(mod[0], mod[1]))
+
+	def do_download(self, arg):
+		"Launches SteamCMD and then quits."
+		subprocess.call([self.steamcdm_path, "+quit"])
+		print("\nMods downloaded\n")
+
+
+if __name__ == '__main__':
+	MyPrompt().cmdloop()
